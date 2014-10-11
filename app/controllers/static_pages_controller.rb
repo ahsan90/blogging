72a9478681d64dadbe518a2b682b220b4a8ad099
Ahsan Rony <ahrony90@gmail.com>
2014-10-11 09:30:33 +0600

    Add user following

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
-    if current_user
-    @micropost = current_user.microposts.build if signed_in?
-    @feed_items = current_user.feed.paginate(page: params[:page]).per_page(10)
+    if signed_in?
+      @micropost = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page]).per_page(10)
     end
   end
 
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -8,4 +8,9 @@ class Micropost < ActiveRecord::Base
     return false unless owner.is_a?(User)
     user == owner
   end
+
+  def self.from_users_followed_by(user)
+    followed_user_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", user_id: user.id)
+  end
 end
